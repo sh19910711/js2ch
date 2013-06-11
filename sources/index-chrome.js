3a5395790b5f31871c33f839c75670377bd9bf40
Hiroyuki Sano <sh19910711@gmail.com>
2013-06-11 22:52:24 +0900

    Edit js2ch-chrome / index-chrome

--- sources/index-chrome.js
+++ sources/index-chrome.js
@@ -3,7 +3,19 @@
 
     var root = this;
 
+    requirejs.config({
+        'paths': {
+            'client': './client',
+            'socket': './socket-chrome',
+            'storage': './storage-chrome',
+            'http': './http',
+            'parser': './parser',
+            'logger': './logger'
+        }
+    });
+
     define([
+        'underscore'
     ], function() {
         // TODO: 実装
         return {};
