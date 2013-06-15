ae2e52825544cafc3c9158ce7f961636f3c9034a
Hiroyuki Sano <sh19910711@gmail.com>
2013-06-16 03:01:49 +0900

    Impl: Node.js用ソケットの雛形を作成

--- sources/socket-node.js
+++ sources/socket-node.js
@@ -4,10 +4,51 @@
     var root = this;
 
     define([
-    ], function() {
-        // TODO: 実装
-        console.log('@socket-node: test');
-        return {};
+        'underscore'
+    ], function(_) {
+        var Socket = function() {
+        };
+
+        Socket.prototype = {};
+        var proto = _(Socket.prototype);
+
+        proto.extend({
+            // ソケットを作成する
+            create: function create(type, options, callback) {
+            }
+        });
+
+        proto.extend({
+            // ソケットを破棄する
+            destroy: function destroy(socketId) {
+            }
+        });
+
+        proto.extend({
+            // 指定のホストに接続する
+            connect: function connect(socketId, hostname, port, callback) {
+            }
+        });
+
+        proto.extend({
+            // 切断する
+            disconnect: function disconnect(socketId) {
+            }
+        });
+
+        proto.extend({
+            // 接続済みのソケットからデータを読み込む
+            read: function read(socketId, bufferSize, callback) {
+            }
+        });
+
+        proto.extend({
+            // 接続済みのソケットにデータを書き込む
+            write: function write(socketId, data, callback) {
+            }
+        });
+
+        return new Socket();
     });
 
 }).call(this);
