fea699feae4a9d3e823486506f382eb261ac112f
Hiroyuki Sano <sh19910711@gmail.com>
2013-06-17 23:00:46 +0900

    Fix: use js-beautify 1.4.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,76 @@
 module.exports = function(grunt) {
   grunt.initConfig({
 
-      requirejs: {
-        buildChrome: {
-          options: {
-            baseUrl: './sources',
-            name: 'index-chrome',
-            mainConfigFile: './sources/index-chrome.js',
-            out: './lib/index-chrome.js',
-            onBuildWrite: function(name, path, contents) {
-              if (name == 'index-chrome') {
-                return contents.replace(/define\(.*'index-chrome'.*,/, 'define(');
-              }
-              return contents;
-            },
-            paths: {
-              jquery: 'empty:',
-              underscore: 'empty:',
-              async: 'empty:',
-              encoding: 'empty:',
-              purl: 'empty:'
+    requirejs: {
+      buildChrome: {
+        options: {
+          baseUrl: './sources',
+          name: 'index-chrome',
+          mainConfigFile: './sources/index-chrome.js',
+          out: './lib/index-chrome.js',
+          onBuildWrite: function(name, path, contents) {
+            if (name == 'index-chrome') {
+              return contents.replace(/define\(.*'index-chrome'.*,/, 'define(');
             }
-          }
-        },
-        buildNode: {
-          options: {
-            baseUrl: './',
-            name: 'sources/index-node',
-            mainConfigFile: './sources/index-node.js',
-            out: './lib/index-node.js',
-            useStrict: true,
-            onBuildWrite: function(name, path, contents) {
-              if (name == 'sources/index-node') {
-                return contents.replace(/define\(.*'sources\/index-node'.*,/, 'define(');
-              }
-              return contents;
-            },
-            paths: {
-              jquery: 'empty:',
-              underscore: 'empty:',
-              async: 'empty:',
-              encoding: 'empty:',
-              purl: 'empty:',
-              net: 'empty:',
-              sqlite3: 'empty:'
-            }
+            return contents;
+          },
+          paths: {
+            jquery: 'empty:',
+            underscore: 'empty:',
+            async: 'empty:',
+            encoding: 'empty:',
+            purl: 'empty:'
           }
         }
       },
-
-      jsbeautifier: {
-        files: ['./sources/*.js', './lib/js2ch-*.js', './Gruntfile.js'],
+      buildNode: {
         options: {
-          indent_size: 2,
-          indent_char: ' ',
-          indent_level: 1,
-          indent_with_tabs: false,
-          preserve_newlines: true,
-          max_preserve_newlines: 10,
-          jslint_happy: false,
-          keep_array_indentation: false,
-          keep_function_indentation: false,
-          space_before_conditional: true,
-          eval_code: false,
-          indent_case: false,
-          unescape_strings: false
+          baseUrl: './',
+          name: 'sources/index-node',
+          mainConfigFile: './sources/index-node.js',
+          out: './lib/index-node.js',
+          useStrict: true,
+          onBuildWrite: function(name, path, contents) {
+            if (name == 'sources/index-node') {
+              return contents.replace(/define\(.*'sources\/index-node'.*,/, 'define(');
+            }
+            return contents;
+          },
+          paths: {
+            jquery: 'empty:',
+            underscore: 'empty:',
+            async: 'empty:',
+            encoding: 'empty:',
+            purl: 'empty:',
+            net: 'empty:',
+            sqlite3: 'empty:'
+          }
         }
       }
+    },
 
-    });
+    jsbeautifier: {
+      files: ['./sources/*.js', './lib/js2ch-*.js', './Gruntfile.js'],
+      options: {
+        indent_size: 2,
+        indent_char: ' ',
+        indent_level: 1,
+        indent_with_tabs: false,
+        preserve_newlines: true,
+        max_preserve_newlines: 10,
+        jslint_happy: false,
+        keep_array_indentation: false,
+        keep_function_indentation: false,
+        space_before_conditional: true,
+        eval_code: false,
+        indent_case: false,
+        unescape_strings: false
+      }
+    }
 
+  });
+
+  grunt.loadTasks('tasks');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-jsbeautifier');
+  // grunt.loadNpmTasks('grunt-jsbeautifier');
 };
