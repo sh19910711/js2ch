00ccc2aa7791be0a1dc6520c7dcdf09813208550
Hiroyuki Sano <sh19910711@gmail.com>
2013-06-17 22:43:04 +0900

    Fix: script formatting

--- sources/buffer-lib-chrome.js
+++ sources/buffer-lib-chrome.js
@@ -11,47 +11,47 @@
   'use strict';
 
   define([
-      'underscore',
-      'encoding',
-      'logger'
-    ], function(_, encoding, logger) {
-      var BufferLib = function() {};
+    'underscore',
+    'encoding',
+    'logger'
+  ], function(_, encoding, logger) {
+    var BufferLib = function() {};
 
-      BufferLib.prototype = {};
-      var proto = _(BufferLib.prototype);
+    BufferLib.prototype = {};
+    var proto = _(BufferLib.prototype);
 
-      proto.extend({
-          // 文字列をArrayBufferに変換する
-          convertToString: function convertToString(buf_array, callback) {
-            var fileReader = new FileReader();
-            fileReader.onloadend = function() {
-              callback(fileReader.result);
-            };
-            fileReader.readAsText(new Blob([new Uint8Array(buf_array)]), 'sjis');
-          }
-        });
-
-      proto.extend({
-          // 文字列をArrayBufferに変換する
-          convertToBuffer: function convertToBuffer(str, callback) {
-            var fileReader = new FileReader();
-            fileReader.onloadend = function() {
-              callback(fileReader.result);
-            };
-            fileReader.readAsArrayBuffer(new Blob([str]));
-          }
-        });
-
-      proto.extend({
-          // 文字列のバイト数を求める
-          getByteLength: function getByteLength(str, callback) {
-            setTimeout(function() {
-              callback(new Blob([str]).size);
-            }, 0);
-          }
-        });
-
-      return new BufferLib();
+    proto.extend({
+      // 文字列をArrayBufferに変換する
+      convertToString: function convertToString(buf_array, callback) {
+        var fileReader = new FileReader();
+        fileReader.onloadend = function() {
+          callback(fileReader.result);
+        };
+        fileReader.readAsText(new Blob([new Uint8Array(buf_array)]), 'sjis');
+      }
     });
+
+    proto.extend({
+      // 文字列をArrayBufferに変換する
+      convertToBuffer: function convertToBuffer(str, callback) {
+        var fileReader = new FileReader();
+        fileReader.onloadend = function() {
+          callback(fileReader.result);
+        };
+        fileReader.readAsArrayBuffer(new Blob([str]));
+      }
+    });
+
+    proto.extend({
+      // 文字列のバイト数を求める
+      getByteLength: function getByteLength(str, callback) {
+        setTimeout(function() {
+          callback(new Blob([str]).size);
+        }, 0);
+      }
+    });
+
+    return new BufferLib();
+  });
 
 }).call(this);
