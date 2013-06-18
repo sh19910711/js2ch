4e6f011b04158da5764951e6bc2c9601cb14cf48
Hiroyuki Sano <sh19910711@gmail.com>
2013-06-19 01:37:05 +0900

    Add tests #3

--- tests/test-node/test-http-lib.js
+++ tests/test-node/test-http-lib.js
@@ -41,25 +41,6 @@
         });
       });
 
-      // "ポート番号が指定してあるURLを読み込むことができない"
-      describe('issue#3 https://github.com/sh19910711/js2ch/issues/3', function() {
-        it('http://localhost:8654/testを取得する', function(done) {
-          // local server
-          var express = require('express');
-          var app = express();
-          app.get('/test', function(req, res) {
-            done.call();
-            return 'test';
-          });
-          app.listen(8654);
-
-          requirejs([
-            'http-lib'
-          ], function(http) {
-            http.get('http://localhost:8654/test', {}, function() {});
-          });
-        });
-      });
     });
 
   });
