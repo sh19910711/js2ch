75933d7e5d0d73a68d8fa0e82535f565a98d2ebc
Hiroyuki Sano <sh19910711@gmail.com>
2013-06-19 04:28:32 +0900

    fixed issue #3

--- tests/test-node/issues/test-3.js
+++ tests/test-node/issues/test-3.js
@@ -15,22 +15,43 @@
         res.setHeader('Content-Length', 4);
         res.end('test');
       });
+      app.get('/fuga', function(req, res) {
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Content-Length', 4);
+        res.end('hoge');
+      });
       app.listen(8654);
     });
 
     // "ポート番号が指定してあるURLを読み込むことができない"
     describe('issue#3 https://github.com/sh19910711/js2ch/issues/3', function() {
+
       it('http://localhost:8654/testを取得する', function(done) {
         requirejs([
           'http-lib'
         ], function(http) {
           http.get('http://localhost:8654/test', {})
             .done(function(res) {
+              res.headers['Status-Code'].should.be.equal('200');
               res.body.should.be.equal('test');
               done();
             });
         });
       });
+
+      it('http://localhost:8654/fugaを取得する', function(done) {
+        requirejs([
+          'http-lib'
+        ], function(http) {
+          http.get('http://localhost:8654/fuga', {})
+            .done(function(res) {
+              res.headers['Status-Code'].should.be.equal('200');
+              res.body.should.be.equal('hoge');
+              done();
+            });
+        });
+      });
+
     });
 
   });
