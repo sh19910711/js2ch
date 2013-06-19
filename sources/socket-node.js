17c6ce73a0a9d421bdc674c9683bd9bf960ac96e
Hiroyuki Sano <sh19910711@gmail.com>
2013-06-19 19:13:43 +0900

    Add jsdoc comments to socket-node

--- sources/socket-node.js
+++ sources/socket-node.js
@@ -7,6 +7,10 @@
  * Licensed under MIT License.
  * http://opensource.org/licenses/MIT
  * =================================================== */
+/**
+ * @fileOverview Node.js用のソケット操作用ライブラリ
+ * @author Hiroyuki Sano
+ */
 (function() {
   'use strict';
 
@@ -20,6 +24,9 @@
   ], function(_, net, encoding, logger) {
     var sockets = [];
 
+    /**
+     * @constructor SocketNode
+     */
     var Socket = function() {};
 
     Socket.prototype = {};
@@ -27,6 +34,17 @@
 
     proto.extend({
       // ソケットを作成する
+      /**
+       * @description ソケットを新しく作成する
+       * @memberof SocketNode
+       *
+       * @param {String} type
+       * ソケットの種類
+       * @param {Options} options
+       * オプション
+       * @param {SocketNode#create-callback} callback
+       * ソケット作成後に callback(Object) として呼び出される
+       */
       create: function create(type, options, callback) {
         var socket = new net.Socket();
         var socket_id = sockets.length;
@@ -37,15 +55,40 @@
       }
     });
 
+    /**
+     * @description ソケット作成後に callback(Object) として呼び出される
+     * @callback SocketNode#create-callback
+     *
+     * @param {Object} socket_info
+     * 作成されたソケットに関する情報
+     */
+
     proto.extend({
-      // ソケットを破棄する
+      /**
+       * @description ソケットを破棄する
+       * @memberof SocketNode
+       *
+       * @param {Integer} socketId
+       */
       destroy: function destroy(socketId) {
         sockets[socketId].destroy();
       }
     });
 
     proto.extend({
-      // 指定のホストに接続する
+      /**
+       * @description 指定のホストに接続する
+       * @memberof SocketNode
+       *
+       * @param {Integer} socketId
+       * ソケットID
+       * @param {String} hostname
+       * ホスト名
+       * @param {Integer} port
+       * ポート番号
+       * @param {SocketNode#connect-callback} callback
+       * ホストへ接続後 callback(Integer) として呼び出される
+       */
       connect: function connect(socketId, hostname, port, callback) {
         var socket = sockets[socketId];
         socket.connect(port, hostname, function() {
@@ -71,15 +114,40 @@
       }
     });
 
+    /**
+     * @description ホストへ接続後 callback(Integer) として呼び出される
+     * @callback SocketNode#connect-callback
+     *
+     * @param {Number} resultCode
+     * 接続が成功したかどうかを表す
+     */
+
     proto.extend({
-      // 切断する
+      /**
+       * @description ソケットの接続を切断する
+       * @memberof SocketNode
+       *
+       * @param {Integer} socketId
+       * 接続を切断するソケットID
+       */
       disconnect: function disconnect(socketId) {
         sockets[socketId].destroy();
       }
     });
 
     proto.extend({
-      // 接続済みのソケットからデータを読み込む
+      /**
+       * @description 接続済みのソケットからデータを読み込む
+       * @memberof SocketNode
+       *
+       * @param {Integer} socketId
+       * データを読み込むソケットID
+       * @param {Integer} bufferSize
+       * バッファサイズ
+       * @param {SocketNode#read-callback} callback
+       * 読み込んだデータをbufferSizeで分割し
+       * 各バッファについて callback(Object) として複数回呼び出される
+       */
       read: function read(socketId, bufferSize, callback) {
         var socket = sockets[socketId];
         var interval_timer = setInterval(function() {
@@ -111,8 +179,28 @@
       }
     });
 
+    /**
+     * @description
+     * 読み込んだデータをbufferSizeで分割し
+     * 各バッファについて callback(Object) として複数回呼び出される
+     * @callback SocketNode#read-callback
+     *
+     * @param {Object} read_info
+     * データや読み込みに関する情報が含まれる
+     */
+
     proto.extend({
-      // 接続済みのソケットにデータを書き込む
+      /**
+       * @description 接続済みのソケットにデータを書き込む
+       * @memberof SocketNode
+       *
+       * @param {Integer} socketId
+       * 書き込みを行うソケットID
+       * @param {Buffer} data
+       * 書き込みを行うデータ
+       * @param {SocketNode#write-callback} callback
+       * 書き込み後に callback() として呼び出される
+       */
       write: function write(socketId, data, callback) {
         var data_str = ArrayBufferToString(data);
         var socket = sockets[socketId];
@@ -121,6 +209,11 @@
         });
       }
     });
+
+    /**
+     * @description ソケットへの書き込み後に callback() として呼び出される
+     * @callback SocketNode#write-callback
+     */
 
     function ArrayBufferToString(buf) {
       return String.fromCharCode.apply(null, new Uint16Array(buf));
